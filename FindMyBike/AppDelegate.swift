6899b9537c64a362e22c386e788f7ca77d4e8327
James Donohue <james.brackish@gmail.com>
2017-09-24 21:53:26 +0100

    Working remote notifications

--- FindMyBike/AppDelegate.swift
+++ FindMyBike/AppDelegate.swift
@@ -41,11 +41,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: Remote notifications
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
-        os_log("Obtained device token for remote notifications: %@", log: log, type: .debug, String(data: deviceToken, encoding: .utf8)!)
+        os_log("Registered for remote notifications", log: log, type: .info)
+
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        os_log("Device token: %@", log: log, type: .info, token)
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         os_log("Remote notifications not available: %@", log: log, type: .error, error.localizedDescription)
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        os_log("Received remote notification: %@", log: log, type: .info, userInfo)
+        completionHandler(.newData)
     }
 }
 
