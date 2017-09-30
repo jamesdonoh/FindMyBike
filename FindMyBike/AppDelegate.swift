88efae43716eb1bb93c50b1cfc9b6191a934e1d6
James Donohue <james.brackish@gmail.com>
2017-09-30 11:55:49 +0100

    Extract lat and long from push notification when opened from background

--- FindMyBike/AppDelegate.swift
+++ FindMyBike/AppDelegate.swift
@@ -7,10 +7,11 @@
 //
 
 import UIKit
+import UserNotifications
 import os.log
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     // MARK: Properties
 
@@ -26,8 +27,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         os_log("didFinishLaunchingWithOptions", log: log, type: .debug)
         os_log("vendor ID = %@", log: log, type: .info, UIDevice.current.identifierForVendor?.description ?? "(none)")
 
-        // Register for push notifications via APNs - note always uses AppDelegate
+        // Register for push notifications via APNs - note mandatory to use AppDelegate
         UIApplication.shared.registerForRemoteNotifications()
+
+        // Assign delegate for user notifications - must be done before application finishes launching
+        UNUserNotificationCenter.current().delegate = self
 
         return true
     }
@@ -40,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         os_log("applicationDidBecomeActive", log: log, type: .debug)
     }
 
-    // MARK: Remote notifications
+    // MARK: Remote notification registration
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data){
         os_log("Registered for remote notifications", log: log, type: .info)
@@ -60,5 +64,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         os_log("Received remote notification: %@", log: log, type: .info, userInfo)
         completionHandler(.newData)
     }
-}
 
+    // MARK: UNUserNotificationCenterDelegate
+
+    // Called when app is in foreground
+//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//        os_log("willPresent: %@", log: log, type: .info, notification.request.content.body)
+//
+//        completionHandler(UNNotificationPresentationOptions.alert)
+//    }
+
+    // Called when app is in background and the user performs an action
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let content = response.notification.request.content
+        os_log("didReceive: %@", log: log, type: .info, content.body)
+
+        guard let latitude = content.userInfo["latitude"] as? Double,
+            let longitude = content.userInfo["longitude"] as? Double else {
+            fatalError("Missing/invalid coordinates in notification")
+        }
+
+        os_log("Sighting at: %f,%f", log: log, type: .info, latitude, longitude)
+
+        completionHandler()
+    }
+}
